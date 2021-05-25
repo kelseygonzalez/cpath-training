7a07177fef29e8f36577bfc378a7e8f620b07b62
Kelsey Gonzalez <kelseygonzalez@email.arizona.edu>
2021-05-25 16:58:24 -0700

    initial commit

---
+++ libs/typed.js
@@ -0,0 +1,17 @@
+// from https://github.com/earowang/rladiesakl20/blob/ae0c26ad457034c69b7630d8e58dea578c26cd26/libs/typed.js
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+
+<script>
+const options = {
+  strings: [
+    '<pre>data["trainer"]</pre>', '<pre>data["researcher"]</pre>', '<pre>data["visualizer"]</pre>'
+  ],
+  typeSpeed: 60,
+  backSpeed: 20,
+  cursorChar: '',
+  loop: true
+};
+
+const typed = new Typed('.typed', options);
+const typed2 = new Typed('.typed-eg', options);
+</script>
